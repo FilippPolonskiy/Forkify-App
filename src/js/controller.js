782b796da92d5adfb8c386f709958d1f97434c78
filipp120697 <filipp.polonskiy@gmail.com>
2022-01-29 17:19:48 +0000

    Cleaned controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -120,9 +120,6 @@ const controlAddRecipe = async function (newRecipe) {
   }
   //Upload the new recipe data
 };
-const newFeature = function () {
-  console.log("Welcome to the app");
-};
 
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
@@ -132,7 +129,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView._addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
 
 init();
